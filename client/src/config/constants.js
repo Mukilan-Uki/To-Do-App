70153b892c8701b61919082edab8d35adf1d73f0
muki-glitch <mvasantharaj1972@gmail.com>
2026-05-24 11:38:04 +0530

    AiChatModel Mobile responsive update

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,3 +1,3 @@
 // Global configuration to easily change the app name and API URL
 export const APP_NAME = "Do(es)";
-export const API_URL ="http://localhost:5000/api";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
